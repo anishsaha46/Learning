0e7b18e8a9c52b959617c880df6257365500ab34
anishsaha46 <anish.ece321046@bppimt.ac.in>
2026-02-26 12:43:28 +0530

    added Course and enrollment imports

--- Course_Selling_Platform/app/db/base.py
+++ Course_Selling_Platform/app/db/base.py
@@ -1,6 +1,11 @@
 from sqlalchemy.orm import DeclarativeBase
 
+# Central Base class for all ORM models
 class Base(DeclarativeBase):
     pass
 
+
+# Import all models here so Alembic can detect them
 from app.models.user import User
+from app.models.course import Course
+from app.models.enrollment import Enrollment
